2742bad8d43634dc9eb13184c3be97b505d0a268
cameronldn <me@cldn.dev>
2025-06-22 15:00:20 +1000

    Added frame rate limited, and delta time tracking

--- main.py
+++ main.py
@@ -3,14 +3,23 @@ from constants import *
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    delta_time = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     gameRunning = True;
+
     while gameRunning:
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
         screen.fill(000)
         pygame.display.flip()
+        delta_time = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
