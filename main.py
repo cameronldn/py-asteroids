1e13b11e7c27b4937e02bf74e4130fdeb616bd09
cameronldn <me@cldn.dev>
2025-06-22 14:46:00 +1000

    Initial setup for Pygame, with basic game loop

---
+++ main.py
@@ -0,0 +1,17 @@
+import pygame
+from constants import *
+
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    print("Starting Asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+    gameRunning = True;
+    while gameRunning:
+        screen.fill(000)
+        pygame.display.flip()
+
+
+if __name__ == "__main__":
+    main()
